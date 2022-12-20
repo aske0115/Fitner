32d639bfea6a3a283b4fa5f140aab810f3668c16
이근화(Geunhwa Lee)/App개발팀/11ST <burzum17@sk.com>
2022-12-20 18:28:25 +0900

    - rename HomeViewController  - make subviews   > 간단한 화면구성 예정중.

--- Fitner/HomeViewController.swift
+++ Fitner/HomeViewController.swift
@@ -1,21 +1,24 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Fitner
 //
 //  Created by Aske on 2022/12/17.
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
-
+    
+    private func initSubviews() {
+        
+    }
 
 }
 
