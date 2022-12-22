49330b5d8db67161ae86fe6deb23e06747adecd7
이근화(Geunhwa Lee)/App개발팀/11ST <burzum17@sk.com>
2022-12-22 17:12:28 +0900

    navigationController 추가 - navigationBar에 설정 바 버튼 추가

--- Fitner/HomeViewController.swift
+++ Fitner/HomeViewController.swift
@@ -13,12 +13,23 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .red
+        initSubviews()
         // Do any additional setup after loading the view.
     }
     
     private func initSubviews() {
-        
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        self.navigationItem.title = "FitStrap"
+        let  settingButton = UIBarButtonItem(image: UIImage(systemName: "gearshape.fill"), style: .plain, target: self, action: #selector(self.pressSettingButton))
+        navigationItem.rightBarButtonItem = settingButton
     }
 
+    
+    @objc func pressSettingButton() {
+        print("press Setting Button")
+    }
 }
 
