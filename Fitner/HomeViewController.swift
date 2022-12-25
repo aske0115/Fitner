a07184a2286c3fdfe13f993ce3467d33f67d72eb
이근화(Geunhwa Lee)/App개발팀/11ST <burzum17@sk.com>
2022-12-25 15:59:28 +0900

    - add addRoutine Button  - reloadDAta

--- Fitner/HomeViewController.swift
+++ Fitner/HomeViewController.swift
@@ -10,6 +10,8 @@ import SnapKit
 
 class HomeViewController: UIViewController {
     
+    private var routines: [String] = ["asda","asdgasdgs"]
+    
     private lazy var collectionView: UICollectionView = {
         var layout = UICollectionViewFlowLayout()
         layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
@@ -23,6 +25,13 @@ class HomeViewController: UIViewController {
         return collectionView
     }()
     
+    private lazy var addRoutineButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.addTarget(self, action: #selector(self.pressAddRoutineButton), for: .touchUpInside)
+        button.setImage(UIImage(systemName: "plus.circle"), for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
@@ -33,18 +42,30 @@ class HomeViewController: UIViewController {
     private func initSubviews() {
         setupNavigationBar()
         makeCollectionView()
+        makeButtons()
     }
     
     private func makeCollectionView() {
         self.view.addSubview(collectionView)
         collectionView.snp.makeConstraints { make in
-            make.edges.equalTo(self.view)
+            make.top.left.right.equalTo(self.view)
+        }
+    }
+    
+    private func makeButtons() {
+        self.view.addSubview(addRoutineButton)
+        addRoutineButton.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(self.view).inset(UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20))
+            make.height.equalTo(50)
+            make.top.equalTo(collectionView.snp.bottom).offset(10)
+            make.bottom.greaterThanOrEqualTo(self.view).priority(750)
         }
     }
     
     private func setupNavigationBar() {
         self.navigationItem.title = "FitStrap"
         let  settingButton = UIBarButtonItem(image: UIImage(systemName: "gearshape.fill"), style: .plain, target: self, action: #selector(self.pressSettingButton))
+        navigationController?.navigationBar.isTranslucent = false
         navigationItem.rightBarButtonItem = settingButton
     }
 
@@ -52,17 +73,22 @@ class HomeViewController: UIViewController {
     @objc func pressSettingButton() {
         print("press Setting Button")
     }
+    
+    @objc func pressAddRoutineButton() {
+        routines.append("\(Int.random(in: 0..<40000))")
+        collectionView.reloadData()
+    }
 }
 
 extension HomeViewController : UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "ProgramCell", for: indexPath) as! ProgramCollectionViewCell
-        cell.configure("asglaskndgls")
+        cell.configure(routines[indexPath.item])
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        routines.count
     }
     
 }
